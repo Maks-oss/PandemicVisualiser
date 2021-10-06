4ca550bbd4edc26738c280a3b02b98510648ceff
maksimus <maksimilianos>
2021-10-06 16:26:00 +0300

    init

---
+++ PandemicVisualiser/CovidFetchRequest.swift
@@ -0,0 +1,13 @@
+//
+//  CovidFetchRequest.swift
+//  PandemicVisualiser
+//
+//  Created by Maksimus on 06.10.2021.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+class CovidFetchRequest:ObservableObject{
+    
+}
